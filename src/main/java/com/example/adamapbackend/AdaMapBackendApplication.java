52b6637e5910a2795318e329b4a8dbbf664ac730
Jaime1401 <jaime14albalate@gmail.com>
2024-04-14 01:22:19 +0200

    initialize

---
+++ src/main/java/com/example/adamapbackend/AdaMapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.adamapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaMapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaMapBackendApplication.class, args);
+	}
+
+}
